0eea5eb8071fd5a29e9da4dc731935fd6cb7807c
nkenon <37303352+nkenon89@users.noreply.github.com>
2018-10-17 18:49:55 -0400

    add html pages for card info and card preview pages

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,9 +1,15 @@
 console.log('javascript file linked successfully');
 
-const signInButton = document.getElementById('signin');
+const crateDesignButton = document.getElementById('createDesign');
+const previewDiv = document.getElementById('previewDiv');
 
 // when sign in button is clicked...
 
-signInButton.addEventListener('click', () => {
+createDesignButton.addEventListener('click', () => {
+    // Pull random template design into previewDiv as bg image
+    // Take information from form input and store it in variables
+    // call that information to dynamically generate html
+    // print html + custom inputs to "preview" div
 
+    // style input using custom CSS to align onto bg image
 })
