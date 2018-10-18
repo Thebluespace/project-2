d0e04c6fab377df2a30f0d10305205bf32911059
Thomas Greene <tecgreene93@gmail.com>
2018-10-17 20:18:35 -0400

    changed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,20 +22,14 @@ module.exports = function(app, passport) {
     }
   });
     app.get('/newUserForm', authController.newUserForm);
-
-
     app.get('/signin', authController.signin);
-
-
     app.post('/newUserForm', passport.authenticate('local-signup', {
         successRedirect: '/choosedesign',
 
         failureRedirect: '/newUserForm'
     }));
     app.get('/choosedesign', isLoggedIn, authController.choosedesign);
-
     app.get('/logout', authController.logout);
-
     function isLoggedIn(req, res, next) {
 
         if (req.isAuthenticated())
