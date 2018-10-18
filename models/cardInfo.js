a2a96a2de803d45017620bbed2e7558314fea345
Tysheba <tmorgan2dev@gmail.com>
2018-10-17 20:28:26 -0400

    add cardInfo table

---
+++ models/cardInfo.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+    var CardInfo = sequelize.define("CardInfo", {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 55]
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        len: [1]
+      },
+      phone: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          len: [1, 10]
+      },
+      website: {
+          type: DataTypes.STRING
+      },
+
+    });
+  
+    CardInfo.associate = function(models) {
+      // We're saying that  CardInfo should belong to a User
+      // CardInfo can't be created without a User due to the foreign key constraint
+      CardInfo.belongsTo(models.users, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return CardInfo;
+  };
+  
